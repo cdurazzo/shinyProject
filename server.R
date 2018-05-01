81f5a113a76345053832782b811a4ecaedc6e55f
cdurazzo <32426056+cdurazzo@users.noreply.github.com>
2018-05-01 16:56:52 -0400

    Fixed graphs, cluster map details, added picture

--- server.R
+++ server.R
@@ -42,11 +42,42 @@ shinyServer(function(input, output) {
  
   output$mymap <- renderLeaflet({
     crime_filter = crime %>%
-      filter(., code == input$selected)
+      filter(., ofns_desc == input$selected)
     
     leaflet(crime_filter) %>%
-      addProviderTiles("Esri.WorldStreetMap") %>%
-      addCircles(~longitude, ~latitude)
+      addProviderTiles("Hydda.Full") %>% #Esri.WorldImagery, Hydda.Full
+      addCircleMarkers(~longitude, ~latitude, opacity = 0.001, color = 'red', radius = 5)
   })
+  
+  output$clustermap <- renderLeaflet({
+    leaflet(crime) %>%
+      addTiles() %>%
+      addProviderTiles("Hydda.Full") %>%
+      addMarkers(~longitude, ~latitude, clusterOptions = markerClusterOptions(), popup = crime$ofns_desc)
+  })
+  
+  output$graph1 <- renderPlot({
+    ggplot(data = crime, aes(x = crime$boro_nm, y = crime$code)) +
+      geom_bar(aes(fill = crime$boro_nm), stat = 'identity') +
+      labs(title = 'Crimes per Borough',
+           x = 'Borough',
+           y = 'Number of crimes occurred')+
+      guides(fill=guide_legend(title="Borough Name"))
+  })
+  
+  output$graph2 <- renderPlot({
+    ggplot(data = pop_data, aes(x=pop_data$boro_nm, y=pop_data$pop_sm))+
+      geom_bar(stat = 'identity', aes(fill =pop_data$boro_nm)) + 
+      labs(title = 'Borough Population Per Square Mile',
+           x = 'Borough',
+           y = 'Population per square mile')+
+      guides(fill=guide_legend(title="Borough Name"))
+  })
+  
+  output$mytable = DT::renderDataTable({
+    crime_table
+  })
+
 }
 )
+
--- ui.R
+++ ui.R
@@ -2,13 +2,15 @@ library(shiny)
 library(data.table)
 
 sidebar <- dashboardSidebar(
+  sidebarUserPanel(name = 'Chris Durazzo', image = 'https://media.licdn.com/dms/image/C5603AQHMHXmknX2EUQ/profile-displayphoto-shrink_200_200/0?e=1530392400&v=beta&t=JSoKewnQmQp-llozUjwrVTCzez5W8MY1PE3vulwRbqw'),
   sidebarMenu(
-    menuItem("Dashboard", tabName = "dashboard", icon = icon("dashboard")),
-    menuItem("Full Dataset", icon = icon("file-code-o"), 
-             href = 'https://data.cityofnewyork.us/Public-Safety/NYPD-Complaint-Data-Historic/qgea-i56i'),
-    selectInput(inputId = "selected",
-                   label = "Select Crime to display",
-                   choices = unique(crime$code), selected = 1)
+    menuItem("Map with Choices", tabName = "dashboard", icon = icon("dashboard")),
+    menuItem("Cluster Map", tabName = "clustermap", icon = icon("map-marker")),
+    menuItem("Graphs", tabName = "graphs", icon = icon("delicious")),
+    menuItem("Data", tabName = "dt", icon = icon("file")),
+    menuItem("Full Dataset", icon = icon("database"), 
+             href = 'https://data.cityofnewyork.us/Public-Safety/NYPD-Complaint-Data-Historic/qgea-i56i')
+    
   )
 )
 
@@ -17,9 +19,26 @@ body <- dashboardBody(
     # First tab content
     tabItem(tabName = "dashboard",
             fluidRow(
-              box(leafletOutput('mymap'))
+              box(leafletOutput('mymap'), background = 'blue'),
+              box(selectInput(inputId = "selected",
+                          label = "Select Crime to display",
+                          choices = unique(crime$ofns_desc), selected = 1), status = 'success', background = 'blue')
             )
-    )
+    ),
+    
+    tabItem(tabName = "clustermap",
+            fluidRow(
+              box(leafletOutput('clustermap'), background = 'blue',width = 8)
+            )),
+    
+    tabItem(tabName = "graphs",
+            fluidRow(
+              box(plotOutput('graph1'), background = 'blue'),
+              box(plotOutput('graph2'), background = 'blue')
+            )),
+    
+    tabItem(tabName = "dt",
+            DT::dataTableOutput("mytable"))
     )
 )
    
