1af79748d606d8c687939809713ba6404e99fab7
cdurazzo <32426056+cdurazzo@users.noreply.github.com>
2018-05-01 22:39:34 -0400

    Update server.R

--- server.R
+++ server.R
@@ -4,39 +4,6 @@ library(tidyr)
 library(ggplot2)
 library(data.table)
 
-# function(input, output){
-#   #takes user input and creates text string to pass on to
-#   formulaText <-reactive({
-#     paste('crime ~',input$Variable)
-#   })
-#   
-#   output$caption <-renderText({
-#     formulaText()
-#   })
-#   
-#   output$crime_plot <- renderPlot({
-# 
-#     ggplot(data = crime, aes(x = crime$boro_nm, y = crime$code)) +
-#       geom_bar(stat = 'identity') +
-#       labs(title = 'Crimes per Borough',
-#            x = 'Borough',
-#            y = 'Crime by desc code') +
-#       scale_color_brewer(palette = 'Blues')
-#     # leaflet(crime) %>%
-#     #   addTiles() %>%
-#     #   addMarkers(~longitude, ~latitude)
-# 
-#     # boxplot(as.formula(formulaText()),
-#     #         data = as.data.frame(crime),
-#     #         outline = input$outliers,
-#     #         col = "#75AADB", pch = 19)
-#   })
-#   
-# }
-
-#choice <- unique(crime$code)
-
-
 
 shinyServer(function(input, output) {
  
